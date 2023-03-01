5b5bebadbb4f55c55e4abd695e5a3af251afb9c1
Guilherme Adam <adam@Guilhermes-MacBook-Pro.local>
2023-03-01 08:34:22 -0300

    Configurando Analytics Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Analytics } from '@vercel/analytics/react';
 export function App() {
   return (
-    <h1>Argum - Landing Page</h1>
+    <>
+      <h1>Argum - Landing Page</h1>
+      <Analytics />
+    </>
   );
 }
